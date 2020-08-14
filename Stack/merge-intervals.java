d9151dce945360b289e13a93fec6229abeb87f4e
Rohit jain <31067636+rj33536@users.noreply.github.com>
2020-08-14 20:17:40 +0530

    Create merge-intervals.java
    https://leetcode.com/problems/merge-intervals/

---
+++ Stack/merge-intervals.java
@@ -0,0 +1,46 @@
+class Solution {
+    public boolean overlap(int[]first,int[]second){
+        if(first[1]>=second[0]){
+            return true;
+        }
+        return false;
+    }
+    public void merge(int[]first,int[]second){
+        first[1] = Math.max(first[1],second[1]);
+    }
+    static class myComparator implements Comparator<int[]>{
+        public int compare(int[]first,int[]second){
+            if(first[0]<second[0]){
+                return -1;
+            }
+            if(first[0]>second[0]){
+                return 1;
+            }
+            
+            return 0;
+        }
+    }
+    public int[][] merge(int[][] intervals) {
+        Arrays.sort(intervals, new myComparator());
+        int i=0,j=1;
+        boolean flag = false;
+        if(intervals.length<=1) return intervals;
+        Stack<int[]>st = new Stack();
+        st.push(intervals[0]);
+        for(j=1;j<intervals.length;j++){
+            int[]top = st.pop();
+            if(overlap(top,intervals[j])){
+                merge(top,intervals[j]);
+                st.push(top);
+            }else{
+                st.push(intervals[j]);
+                intervals[i++] = top;
+                
+            }
+        }
+        int[]top = st.pop();
+        intervals[i++] = top;
+        
+        return Arrays.copyOfRange(intervals,0,i);
+    }
+}
